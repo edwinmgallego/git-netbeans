dfdd3ae5b59d26b2a13953300cfb1a3b4628ca4e
edwinmgallego <ing.edwin.gallego@gmail.com>
2026-09-16 08:42:39 -0500

    creamos  el proyecto , y este es mi primer commit

---
+++ src/main/java/com/mycompany/clasedegitgithub/ClaseDeGitgithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasedegitgithub;
+
+/**
+ *
+ * @author emgallego
+ */
+public class ClaseDeGitgithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
