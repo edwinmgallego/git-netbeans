d47b2e8a947ee027406af9812e5a51d5f0526eaf
usuario <dpinomarin@gmail.com>
2026-09-16 09:45:06 -0500

    creamos el proyecto y este es mi commit se hizo otro print y otro print se hizo en la rama de daniel en el archivo del profe4

--- src/main/java/com/mycompany/clasedegitgithub/ClaseDeGitgithub.java
+++ src/main/java/com/mycompany/clasedegitgithub/ClaseDeGitgithub.java
@@ -11,8 +11,9 @@ package com.mycompany.clasedegitgithub;
 public class ClaseDeGitgithub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
         System.out.println("Hello World1!");
+        System.out.println("Hello World!");
         System.out.println("Hello World2!");
         System.out.println("Hello World3!");
         System.out.println("Hello World4!");
